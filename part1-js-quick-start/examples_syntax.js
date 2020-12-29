416dca7949e01e041e7fa6704f40bf9fd544ce32
CookiePark666 <74982206+CookiePark666@users.noreply.github.com>
2020-12-29 11:57:18 +0000

    Create examples_syntax.js

---
+++ part1-js-quick-start/examples_syntax.js
@@ -0,0 +1,19 @@
+// 2 slashes start single-line comments
+var x; // declaring a variable
+x = 3 + y; // assigning a value to the variable 'x'
+
+foo(x,y); // calling function 'foo' with parameters 'x' and 'y'
+obj.bar(3); // calling method 'bar' of object 'obj'
+
+// a conditional statement
+if(x === 0){// is 'x' equal to 0?
+  x = 123;
+}
+// Defining function 'baz' with parameters 'a' and 'b'
+function baz(a,b){
+  return a+b;
+}
+
+// a single equals sign is used to assign a value to a variable
+// a triple equals sign is used to compare 2 values
+
